db89dc7b87f771b3510e59cd2f0546bf87c0ee48
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-12 14:09:24 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this function prints lower case alphabets.
+ *
+ * Return: should be 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	_putchar('\n');
+	}
+}
