07e79340df83f4556eb201224b55952a50f338d9
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-12 16:26:06 -0700

    Last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: parameter to be checked
+ * Return: x
+ */
+
+int print_last_digit(int a)
+{
+	int x;
+
+
+	x = a % 10;
+	if (a < 0)
+		x = -a;
+	_putchar(x + '0');
+	
+	return (x);
+}
