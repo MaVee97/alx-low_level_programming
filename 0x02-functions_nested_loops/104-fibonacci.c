9b033daf7380d856844eb55c5e6cc79408e65441
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-13 12:35:11 -0700

    Fib 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the first 98 Fibonacci numbers
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	int x;
+	unsigned long int next;
+
+	printf("%lu, %lu", num1, num2);
+
+	for (x = 2 ; x < 98 ; x++)
+	{
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
+		printf(", %lu", next);
+	}
+	printf("\n");
+	return (0);
+}
