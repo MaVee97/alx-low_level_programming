ed1301699653bc2832afdce942096ab59bf35c4b
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-14 17:28:06 -0700

    Yeer

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,14 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 1 ; x <= 14 ; x++)
+	for (x = 0 ; x < 10 ; x++)
 	{
 
-		for (y = 0 ; y < 10 ; y++)
+		for (y = 0 ; y <= 14 ; y++)
 		{
-			_putchar((x % 10) + '0');
+			if (y > 9)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
