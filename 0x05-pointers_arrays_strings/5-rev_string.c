8b68f7ea7fd9744ba28d3696a849fc1b57ec3d86
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-18 16:50:34 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: functon parameter
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	char y;
+	int count = 0;
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+	{
+		count++;
+	}
+	for (x = 0 ; x < count / 2 ; x++)
+	{
+		y = s[x];
+		s[x] = s[count - 1 - x];
+		s[count - 1 - x] = y;
+	}
+}
