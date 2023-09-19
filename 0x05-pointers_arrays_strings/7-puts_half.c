edc50a1196cd1756a50fee0e924ad20222182f77
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-18 17:17:30 -0700

    Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: function parameter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int x, n, count = 0;
+
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		count++;
+	}
+
+	n = (count - 1) / 2;
+
+	for (x = n + 1 ; str[x] != '\0' ; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
+
