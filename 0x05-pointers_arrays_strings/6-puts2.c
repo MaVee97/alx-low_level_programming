ea248f501f634e2dc2116d86ef9dd412ba06bd8c
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-18 17:01:02 -0700

    Even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: function parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+
+	}
+	_putchar('\n');
+}
