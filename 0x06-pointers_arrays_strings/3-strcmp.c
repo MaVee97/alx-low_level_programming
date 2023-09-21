d3dd06abede5bb9ee856f597cc15fd3641224acf
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-09-21 06:09:43 -0700

    Compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: function parameter
+ * @s2: function parameter
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for (x = 0 ; s1[x] != '\0' || s2[x] != '\0' ; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			if (s1[x] < s2[x])
+			{
+				return (s1[x] - s2[x]);
+			}
+			else if (s1[x] > s2[x])
+			{
+				return (s1[x] - s2[x]);
+			}
+		}
+		else
+			return (0);
+	}
+	return ('\n');
+}
