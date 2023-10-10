b0f3f3c6b944c4e6ff8bf3bf33cb82fab4650845
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-10-09 17:53:28 -0700

    Woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
