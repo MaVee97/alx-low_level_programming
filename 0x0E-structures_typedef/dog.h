b96ff2b89b1e492d83d1bfc64a2e6fbc6e09ed35
Vutlhari Wisdom Makhubele <wmakhubele10@gmail.com>
2023-10-09 18:23:41 -0700

    Easy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
